b673ae8ac9e8be59a656ba32b43ba5dd4a7c49df
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-26 17:13:04 +0300

    feat: convinient methods added to teacher entity and one to many relation annotation added to set

--- src/main/java/gr/aueb/cf/model/Teacher.java
+++ src/main/java/gr/aueb/cf/model/Teacher.java
@@ -2,6 +2,15 @@ package gr.aueb.cf.model;
 
 import jakarta.persistence.*;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Χρησιμοποιούμε το API του hibernate
+ * με annotations πανω απο κάθε πεδίο
+ */
+
 @Entity
 @Table(name = "teachers")
 public class Teacher {
@@ -17,17 +26,24 @@ public class Teacher {
     @Column(nullable = false)
     private String lastname;
 
-    //    private Set<Course> courses;  Για να υλοποιήσω την σχέση 1 προς πολλά με τα courses
+
+    // Για να υλοποιήσω τη σχέση 1 προς πολλά με τα courses
+    // Τα hashset έχουν καλύτερους χρόνους αναζήτησης O(1)
+    @OneToMany(mappedBy = "teacher")   // Το στοιχείο που έχουμε στην άλλη μεριά (στο course έχουμε πεδίο teachers)
+    private Set<Course> courses;
 
     public Teacher() {
 
     }
 
-    public Teacher(Long id, String firstname, String lastname) {
+    public Teacher(Long id, String firstname, String lastname, Set<Course> courses) {
         Id = id;
         this.firstname = firstname;
         this.lastname = lastname;
+        this.courses = courses;
     }
+
+
 
     public Long getId() {
         return Id;
@@ -51,5 +67,41 @@ public class Teacher {
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    // Το βάζουμε protected γιατί επιστρέφουμε reference
+    // και τα instances courses δεν είναι immutable
+    protected Set<Course> getCourses() {
+        return courses;
+    }
+
+    // Μόνο view μπορεί να το κάνει αυτό λόγο
+    // του unmodifiable set
+    public Set<Course> getAllCourses() {
+        return Collections.unmodifiableSet(courses);
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+
+    public void addCourse(Course course) {
+        if (courses == null) courses = new HashSet<>();
+        courses.add(course);
+        course.setTeacher(this);
+    }
+
+    public void removeCourse(Course course) {
+        courses.remove(course);
+        course.setTeacher(null);
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "Id=" + Id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
     }
 }
