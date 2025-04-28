8417b603a7e507b1dc7edc49b624c23a7a994069
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-28 18:04:34 +0300

    feat: select queries added on app, most commented out

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -6,6 +6,10 @@ import gr.aueb.cf.model.Teacher;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+import java.util.Objects;
 
 public class App {
 
@@ -27,8 +31,8 @@ public class App {
         // Εδώ ξεκινάει το transaction, δηλαδή σειρά crud πράξεων που πρέπει να γίνουν όλες ή καμία
         em.getTransaction().begin();
 
-        Course course = em.find(Course.class, 2L);                            // Βρίσουμε πρώτα το στοιχείο που ψάχνουμε (databases που ήταν το 2L)
-        em.remove(course);                                                       // Αφαιρούμε το course
+//        Course course = em.find(Course.class, 2L);                            // Βρίσουμε πρώτα το στοιχείο που ψάχνουμε (databases που ήταν το 2L)
+//        em.remove(course);                                                     // Αφαιρούμε το course
 
  //       Teacher alice = em.find(Teacher.class, 1L);                            // Τραβάμε την alice πχ που βάλαμε πριν (id = 1L)
  //       Course databases = new Course(null, "Databases", null);                // Δημιουργία "Databases" μάθημα
@@ -39,6 +43,44 @@ public class App {
 
 //        em.persist(teacher);                                                   // Εισαγωγή entity στο PersistentContext, το αποθηκεύει στην ΒΔ με commit
 //        em.persist(java);                                                      // Εισαγωγή entity στο PersistentContext, το αποθηκεύει στην ΒΔ με commit
+
+
+        // Select όλους τους teachers
+//        String sql = "SELECT t FROM Teacher t";
+//        // Προσφέρει compile-time safety
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        List<Teacher> teachers = query.getResultList();
+//        teachers.forEach(System.out::println);
+
+        // Select όλα τα courses
+//        String sql = "SELECT c FROM Course c";
+//        List<Course> courses = em.createQuery(sql, Course.class).getResultList();
+//        courses.forEach(System.out::println);
+
+        // Select courses που διδάσκει ο Μ. Καραμπάτσης
+
+//        String sql = "SELECT c FROM Course c WHERE c.teacher.firstname = :firstname"; // ψευδώνυμο, μπορεί και αριθμιτικό να είναι ?1 (πρώτος) / Το κάνουμε λόγο SQL injection attack
+//        TypedQuery<Course> query = em.createQuery(sql, Course.class);
+//        query.setParameter("firstname", "Μάρκος");
+//        List<Course> courses = query.getResultList();
+
+//        List<Course> courses = em.createQuery(sql, Course.class)
+//                        .setParameter("firstname", "Μάρκος")
+//                        .getResultList();
+//        courses.forEach(System.out::println);
+
+        // Select teachers και τίτλους courses που διδάσκουν οι teachers
+        String sql = "SELECT t, c.title FROM Teacher t JOIN t.courses c"; // INNER JOIN
+        TypedQuery<Object[]> query = em.createQuery(sql, Object[].class);
+        List<Object[]> results = query.getResultList();
+
+        for (Object[] result : results) {
+            Teacher teacher = (Teacher) result[0];
+            String courseTitle = (String) result[1];
+            System.out.println("Teacher: " + teacher.getLastname() + ", Course: " + courseTitle);
+        }
+
+
 
         em.getTransaction().commit();                                            // Αποθήκευση στη βάση δεδομένων
 
